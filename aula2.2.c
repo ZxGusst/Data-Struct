8571926b2149d5498f22b5c5d84695322a88f118
Gustavo dos Santos Alves <gustavosantoos.gg@gmail.com>
2024-03-22 11:26:34 -0300

    Add files via upload

--- aula2.2.c
+++ aula2.2.c
@@ -4,8 +4,15 @@
 
 #define ERRO -1234
 
-int F(int N );
+int Mystrlen(char *str);
 
 void main(){
-    
+    char frase[] = "Meu nome e Gustavo!!!";
+    printf("%s - %d caracteres.\n", frase, MyStrlen(frase));
+}
+
+int Mystrlen (char *str){
+    int Tamanho;
+    for (Tamanho=0; str[Tamanho] != '\0';Tamanho++);
+    return(Tamanho);
 }
